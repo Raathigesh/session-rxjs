38c4cbdd3e5e4a0431cb94dfbeab16371fd90001
ZONE24X7-CMB\raathigeshank <raathigeshank@zone24x7.com>
2017-04-01 16:34:58 +0530

    Rxjs advanced example

---
+++ rxjsMultipleStreams/script.js
@@ -0,0 +1,21 @@
+Rx.Observable
+  .fromEvent(document.getElementById("search"), "keyup")
+  .switchMap(event => {
+    return Rx.Observable.fromPromise(
+      axios.get("http://localhost:3334/movies/" + event.target.value)
+    );
+  })
+  .subscribe(results => {
+    renderResults(results.data);
+  });
+
+function renderResults(results) {
+  const searchResultsElement = document.getElementById("searchResults");
+  searchResultsElement.innerHTML = "";
+
+  for (let item of results) {
+    const el = document.createElement("li");
+    el.innerHTML = item;
+    searchResultsElement.appendChild(el);
+  }
+}
